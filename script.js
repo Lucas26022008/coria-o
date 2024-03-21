f9c593563914892387a0112f9c65f6b64ba245fd
Lucas Dircksen Balcevicz <145569604+Lucas26022008@users.noreply.github.com>
2024-03-21 13:47:14 -0300

    .

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('card').addEventListener('click', function() {
+    // Adiciona corações
+    for (let i = 0; i < 10; i++) {
+      createHeart();
+    }
+  });
+  
+  function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = Math.random() * 2 + 3 + 's';
+    heart.innerText = '❤️';
+    document.body.appendChild(heart);
+  
+    setTimeout(() => {
+      heart.remove();
+    }, 5000);
+  }
+  
