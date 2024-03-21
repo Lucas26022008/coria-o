28aadfcfafb70ea2f9da761d46d7cdb455696b70
Lucas Dircksen Balcevicz <145569604+Lucas26022008@users.noreply.github.com>
2024-03-21 13:50:08 -0300

    .

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ document.getElementById('card').addEventListener('click', function() {
   function createHeart() {
     const heart = document.createElement('div');
     heart.classList.add('heart');
-    heart.style.left = Math.random() * 100 + 'vw';
-    heart.style.animationDuration = Math.random() * 2 + 3 + 's';
+    heart.style.left = Math.random() * 90 + 'vw';
     heart.innerText = '❤️';
     document.body.appendChild(heart);
   
